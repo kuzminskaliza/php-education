1287911ac9d157dcf795fdcf0a71bd98efbe1e7b
Liza <kuzminskaliza2017@gmail.com>
2024-05-23 11:39:21 +0300

    PHP-3_school

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-echo "Hello World";
-$a=1;
-$a=1;
-$a=1;
-$a=1;
-$a=2;
-$a=0;
-$a=3;
+echo "My first PHP script!";
+
+//This is a single-line comment
+#This is also a single-line comment
+
+/*This is a
+ multi-line
+ comment*/
+
+$x= 5;
+$y= 12;
+echo $x + $y;
+
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+
+print "I'm about to learn PHP!";

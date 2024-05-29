d3eb9b255d7ff0cb845bdb868cb649731044300b
Liza <kuzminskaliza2017@gmail.com>
2024-05-29 15:41:30 +0300

    PHP-7_statements

---
+++ statements.php
@@ -0,0 +1,28 @@
+<?php
+$t = 14;
+
+if ($t < 20) {
+    echo "Have a good day!";
+}
+
+if ($t == 14) {
+    echo "Have a good day!";
+}
+
+$a = 200;
+$b = 33;
+$c = 500;
+
+if ($a > $b && $a < $c ) {
+    echo 'Both conditions are true'; //якщо a > b i a < c то тоді умови правильні
+}
+
+$a = 5;
+
+if ($a == 2 || $a == 3 || $a == 4 || $a == 5 || $a == 6 || $a == 7) {
+    echo '$a is a number between 2 and 7';
+}
+
+
+
+

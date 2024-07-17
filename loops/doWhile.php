fbc9ebdf27bd1c8852894268cfe3e92cda2dbd1d
Liza <kuzminskaliza2017@gmail.com>
2024-07-17 17:45:18 +0300

    loops

--- loops/doWhile.php
+++ loops/doWhile.php
@@ -33,3 +33,15 @@ do {
     if ($i == 3) continue;
     echo $i;
 } while ($i < 6);
+
+$r = 10;
+do {
+    echo 2 . PHP_EOL;
+    $r++;
+} while ($r < 10);
+
+$number = 5;
+do {
+    echo "Value of number is: $number\n";
+    $number++;
+} while ($number <= 9);
--- loops/while.php
+++ loops/while.php
@@ -27,3 +27,21 @@ while ($i < 6) {
     if ($i == 3) continue;
     echo $i;
 }
+
+
+$r = 0;
+while ($r < 10) {
+    echo 2 . PHP_EOL;
+    $r++;
+}
+
+$x = 10;
+$y = 0;
+
+while ($y<=10) {
+    $y += $x;
+    $x += 1;
+}
+echo "the value of x is: $x\n";
+echo "the value of y is: $y\n";
+

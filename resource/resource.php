ae4f8c27e7e890dbba7c867a7573c12e8309f866
Liza <kuzminskaliza2017@gmail.com>
2024-08-04 21:48:10 +0300

    PHP-27

--- resource/resource.php
+++ resource/resource.php
@@ -6,3 +6,23 @@ $a = fopen($file, 'r+');
 echo get_resource_type($a); //виведе stream, який використовується для читання або запису даних
 fclose($a);
 
+
+//функція перевіряє чи є змінна ресурсом
+$b = fopen($file, 'r+');
+if (is_resource($b)) {
+    echo "змінна $b є ресурсом ";
+    fclose($b);
+} else {
+    echo "Змінна $b не є ресурсом";
+}
+
+
+//функція повертає унікальний індекс ресурсу
+$c = fopen($file, 'r+');
+if ($c){
+    $resourceId = get_resource_id($c);
+    echo 'Ідентифікатор ресурсу' . $resourceId;
+    fclose($c);
+} else {
+    echo 'Невдалось відкрити файл';
+}

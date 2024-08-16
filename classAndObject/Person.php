a33328fe967c1364354ba1e7175bd9f897a0afb4
Liza <kuzminskaliza2017@gmail.com>
2024-08-16 10:21:51 +0300

    PHP-28

--- classAndObject/Person.php
+++ classAndObject/Person.php
@@ -1,28 +1,47 @@
 <?php
 
+/**
+ *
+ */
 class Person
 {
+
     /**
      * @var string|null
      */
     public ?string $name = 'Liza';
+    /**
+     * @var int
+     */
     public int $age;
 
+    /**
+     *
+     */
     public function __construct()
     {
         $this->age = 20;
     }
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * @return string
+     */
     public function getPerson(): string
     {
         return "Name: " . $this->getName() . PHP_EOL . "Age: $this->age";
     }
 
+    /**
+     * @return int
+     */
     public function getAge(): int
     {
         return $this->age;

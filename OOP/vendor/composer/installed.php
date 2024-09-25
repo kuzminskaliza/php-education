797007f66e2126b7c86afeb254e4afc8e10d1ab3
Liza <kuzminskaliza2017@gmail.com>
2024-09-25 11:27:18 +0300

    PHP-42

---
+++ OOP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8c81d881d1ae11cb65e4a3869e80b37e50816a06',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8c81d881d1ae11cb65e4a3869e80b37e50816a06',
+            'dev_requirement' => false,
+        ),
+    ),
+);

3d3ad048093c11e7e096e9705bb1dcf7840c1b31
bursak <bursak@rozetka.ua>
2025-04-22 14:05:04 +0300

    tes

--- traits/ElectricCars.php
+++ traits/ElectricCars.php
@@ -12,10 +12,20 @@ class ElectricCars
     public array $electric2 = [];
     public array $electric3 = [];
     public array $electric4 = [];
+    public array $cars = [];
+    public array $cars1 = [];
+    public array $cars2 = [];
+    public array $cars3 = [];
+    public array $cars4 = [];
 
     public function charging(): void
     {
         echo 'Заряджання електомобіля';
+    }
+
+    public function addCar(Car $car): void
+    {
+        $this->cars[] = $car;
     }
 }
 $electricCar = new ElectricCars();
